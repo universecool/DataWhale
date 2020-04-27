23e3030efafb9e3f05a4645a4e8364bbbcc44501
univeresecool <universe.cool@163.com>
2020-04-27 23:36:30 +0800

    task03

--- 计算机视觉基础：图像处理（上）/task03/task03.cpp
+++ 计算机视觉基础：图像处理（上）/task03/task03.cpp
@@ -4,20 +4,22 @@
 
 using namespace cv;
 
-int main( int argc, char** argv )
+int main(int argc, char** argv)
 {
     // Load image
     Mat srcImage = imread("1.jpg"), dstImage;
-    
+
     // RGB2GHSV
     cvtColor(srcImage, dstImage, COLOR_BGR2HSV);
     imshow("Lab Space", dstImage);
-    
+    imwrite("Lab_space.jpg", dstImage);
+
     //RGB2GRAY
     cvtColor(srcImage, dstImage, COLOR_BGR2GRAY);
     imshow("Gray Scale", dstImage);
-    
+    imwrite("Gray_scale.jpg", dstImage);
+
     waitKey();
-    
+
     return 0;
 }
